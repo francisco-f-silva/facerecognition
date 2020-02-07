cf68819644fd0603f3751432fc362ad7b1d73874
Francisco Silva <franciscocpfsilva_fcp@hotmail.com>
2020-02-07 10:00:22 +0000

    Added background & Navigation and Logo components & Imported tachyons (for styling) and react-tilt (for logo animation)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+          <Navigation />
+          <Logo />
         {
-          //<Navigation />
-          //<Logo />
           //<ImageLinkForm />
           //<FaceRecognition />
         }
