21108f5f0ba48619dc59b776331e3fd8c2284a99
Francisco Silva <franciscocpfsilva_fcp@hotmail.com>
2020-02-12 10:54:34 +0000

    Connected Register component to /register endpoint & Created user state in App.js and updated it on Register component

--- src/App.js
+++ src/App.js
@@ -34,8 +34,25 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }});
   }
 
   calculateFaceLocation = (data) => {
@@ -52,7 +69,7 @@ class App extends Component {
   }
 
   displayFaceBox = (box) => {
-    console.log(box);
+    //console.log(box);
     this.setState({box: box});
   }
 
@@ -101,7 +118,7 @@ class App extends Component {
             : (
                 this.state.route === 'signin'
                   ? <SignIn onRouteChange={this.onRouteChange} />
-                  : <Register onRouteChange={this.onRouteChange} />
+                  : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
               )
           }
       </div>
